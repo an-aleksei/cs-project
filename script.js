cefd547ed67e7914bfbf59f0fb8555f5eb9e6a54
an-aleksei <aan@pcs-nyc.org>
2025-01-10 11:58:00 -0500

    dark mode for popup as well

--- script.js
+++ script.js
@@ -3,12 +3,14 @@
 function darkModeToggle() {    //adding toggle for dark mode (add some other colors later)
     let element = document.body;
     element.classList.toggle("dark-mode");
+
+    let popup1 = document.getElementById('popup');
+    popup1.classList.toggle('dark-mode');
   }
   // script.js
 const popup = document.getElementById('popup');
 const navbar = document.getElementById('navbar');
 
-// Скрыть всплывающее сообщение и показать navbar при прокрутке вниз
 window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY;
 
@@ -20,3 +22,4 @@ window.addEventListener('scroll', () => {
         navbar.classList.add('hidden'); // Скрыть navbar
     }
 });
+s
